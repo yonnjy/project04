7599310b4f8f68cfe6207199a046192be22f7a47
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-01 18:17:02 +0900

    230601
    230601

--- js/main.js
+++ js/main.js
@@ -14,4 +14,50 @@ $(function () {
     $('.main_vi .play').on('click', function () {
         vdo.YTPPlay();
     });
+
+    $('.story_slide').slick({
+        arrows: false,
+        fade: true,
+        speed: 2000,
+    });
+
+    $('.main_story .arrows .left').on('click', function () {
+        $('.story_slide').slick('slickPrev');
+    });
+    $('.main_story .arrows .right').on('click', function () {
+        $('.story_slide').slick('slickNext');
+    });
+
+    // ?
+    $('.main_story ul li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $('.stroy_slide').slick('slickGoTo', idx);
+    });
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        console.log(sct);
+        if (sct > 0) {
+            $('.header').addClass('on');
+        } else {
+            $('.header').removeClass('on');
+        }
+    });
+
+    $('.story_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('.to_top').fadeIn(50);
+        } else {
+            $('.to_top').fadeOut(50);
+        }
+    });
 });
